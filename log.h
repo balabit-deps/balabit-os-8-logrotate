e5c753a71f65bde24c1dd38fd5fb488111eea2ce
Kócsó Balázs <kocsob@balabit.hu>
2020-12-15 19:14:56 +0100

    Import Upstream version 3.14.0

---
+++ log.h
@@ -0,0 +1,25 @@
+#ifndef H_LOG
+#define H_LOG
+
+#include <stdio.h>
+
+#define MESS_REALDEBUG	1
+#define MESS_DEBUG	2
+#define MESS_VERBOSE	3
+#define MESS_NORMAL	4
+#define MESS_ERROR	5
+#define MESS_FATAL	6
+
+#define LOG_TIMES	(1 << 0)
+
+void message(int level, const char *format, ...)
+#ifdef __GNUC__
+    __attribute__ ((format(printf, 2, 3)));
+#else
+;
+#endif
+void logSetMessageFile(FILE * f);
+void logToSyslog(int enable);
+void logSetLevel(int level);
+
+#endif
